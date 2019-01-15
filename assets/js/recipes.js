487cfaae2e90015cbd5f682afd82e157e4aad317
Jacob Hershberger <whyisjacob@gmail.com>
2019-01-14 17:25:56 -0700

    made initial build

---
+++ assets/js/recipes.js
@@ -0,0 +1,97 @@
+var recipes = [{
+		name: "Spaghetti",
+		meal: 3
+	},
+	{
+		name: "Lasagna",
+		meal: 3
+	},
+	{
+		name: "Cheese Soup",
+		meal: 3
+	},
+	{
+		name: "Potato Cheese Soup",
+		meal: 3
+	},
+	{
+		name: "Tater Tot Casserole",
+		meal: 3
+	},
+	{
+		name: "Chicken and Fries",
+		meal: 3
+	},
+	{
+		name: "Hamburgers",
+		meal: 3
+	},
+	{
+		name: "French Toast",
+		meal: 1
+	},
+	{
+		name: "Breakfast for Dinner",
+		meal: 3
+	},
+	{
+		name: "Flapjacks",
+		meal: 1
+	},
+	{
+		name: "Cereal",
+		meal: 1
+	},
+	{
+		name: "German Pancakes",
+		meal: 1
+	},
+	{
+		name: "Mac 'N Cheese W/ Hamburger",
+		meal: 3
+	},
+	{
+		name: "Gourmet Nachos",
+		meal: 3
+	},
+	{
+		name: "Tacos",
+		meal: 3
+	},
+	{
+		name: "Green Chili Burritos",
+		meal: 3
+	},
+	{
+		name: "Baked Potatoes w/ Chili",
+		meal: 3
+	},
+	{
+		name: "Loaded Potatoes (mashed, cheese, bacon)",
+		meal: 3
+	},
+	{
+		name: "Spaghetti and Chili",
+		meal: 3
+	},
+	{
+		name: "Tortellini",
+		meal: 3
+	},
+	{
+		name: "Pizza Rolls (Totino)",
+		meal: 3
+	},
+	{
+		name: "Shredded Chicken Burritos",
+		meal: 3
+	},
+	{
+		name: "shredded chicken tacos",
+		meal: 3
+	}, 
+	{
+		name: "Chicken Pineapple Quesadillas",
+		meal: 3
+	}
+];
